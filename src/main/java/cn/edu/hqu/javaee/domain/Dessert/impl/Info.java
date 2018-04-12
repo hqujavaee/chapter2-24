4616db2471b7c2c1039d4251b236214bdd5d4983
27597581 <27597581@qq.com>
2018-04-12 18:48:44 +0800

    第一次提交

---
+++ src/main/java/cn/edu/hqu/javaee/domain/Dessert/impl/Info.java
@@ -0,0 +1,10 @@
+package cn.edu.hqu.javaee.domain.Dessert.impl;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Info {
+	public String name="张三";
+	public String location="KFC";
+
+}
